fb1fa2de7bf03fcc464a3ed10efa57a807750a78
Abby Smith <43543823+Smiffy119@users.noreply.github.com>
2019-12-09 17:03:11 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,22 +1,44 @@
 CREATE DATABASE IF NOT EXISTS assignment_2;
 
-CREATE TABLE Customer(
-    Customer_Email varchar(50) PRIMARY KEY,
-    Customer_FirstName char(20),
-    Customer_LastName char(20),
-    Customer_HouseNo int(10),
-    Customer_City char(20),
-    Customer_PostCode varchar(10));
+CREATE TABLE IF NOT EXISTS Customers(
+    Customer_Email varchar(40) NOT NULL PRIMARY KEY,
+    Customer_FirstName varchar(20) NOT NULL,
+    Customer_LastName varchar(20) NOT NULL);
     
-    CREATE TABLE Services(
-    Service_Name char(30) PRIMARY KEY,
-    Service_Description char(100),
-    Service_Price int(5));
+CREATE TABLE IF NOT EXISTS Customer_Addresses(
+    Customer_Email varchar(40) NOT NULL,
+    HouseNo int(10) NOT NULL,
+    City varchar(30)NOT NULL,
+    Postcode varchar(10) NOT NULL,
+    FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email));
     
-    CREATE TABLE Orders(
-    Customer_Email varchar(50),
-    Order_Date_Time DATETIME,
-    Staff_Email varchar(50),
-    PRIMARY KEY (Customer_Email, Order_Date_Time));
+ CREATE TABLE IF NOT EXISTS Services(
+    Service_Name char(30) NOT NULL PRIMARY KEY,
+    Service_Description char(100) NOT NULL,
+    Service_Price int(5) NOT NULL);
     
+CREATE TABLE IF NOT EXISTS Orders(
+    Customer_Email varchar(50) NOT NULL,
+    Order_Date_Time DATETIME NOT NULL,
+    Staff_Email varchar(50) NOT NULL,
+    PRIMARY KEY (Customer_Email, Order_Date_Time),
+    FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email));
     
+ CREATE TABLE IF NOT EXISTS Staff(
+    Staff_Email varchar(40) NOT NULL PRIMARY KEY,
+    Staff_FirstName varchar(20) NOT NULL,
+    Staff_LastName varchar(20) NOT NULL);
+    
+///////////////DOESN'T WORK///////////////////////////////////////////////
+CREATE TABLE IF NOT EXISTS Order_Details(
+    Customer_Email varchar(50) NOT NULL,
+    Order_Date_Time DATETIME NOT NULL,
+    Service_Name varchar(30) NOT NULL,
+    Service_Ordered_Price int(5) NOT NULL, 
+    Discount varchar (30) NOT NULL, 
+    Quantity int(5) NOT NULL,
+    PRIMARY KEY (Customer_Email, Order_Date_Time, Service_Name),
+    FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email),
+    FOREIGN KEY (Order_Date_Time) REFERENCES Orders(Order_Date_Time),
+    FOREIGN KEY (Service_Name) REFERENCES Services(Service_Name));
+//////////////////////////////////////////////////////////////////////////////
