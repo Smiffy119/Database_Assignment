de4a5ba72560adeab2217d113e4d1f07fd58b9d7
Abby Smith <43543823+Smiffy119@users.noreply.github.com>
2019-12-10 10:34:47 +0000

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,7 +1,8 @@
 CREATE DATABASE IF NOT EXISTS assignment_2;
+USE assignment_2;
 
 CREATE TABLE IF NOT EXISTS Customers(
-    Customer_Email varchar(40) NOT NULL PRIMARY KEY,
+    Customer_Email varchar(40) NOT NULL UNIQUE PRIMARY KEY,
     Customer_FirstName varchar(20) NOT NULL,
     Customer_LastName varchar(20) NOT NULL);
     
@@ -13,23 +14,22 @@ CREATE TABLE IF NOT EXISTS Customer_Addresses(
     FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email));
     
  CREATE TABLE IF NOT EXISTS Services(
-    Service_Name char(30) NOT NULL PRIMARY KEY,
+    Service_Name char(30) NOT NULL UNIQUE PRIMARY KEY,
     Service_Description char(100) NOT NULL,
     Service_Price int(5) NOT NULL);
     
 CREATE TABLE IF NOT EXISTS Orders(
     Customer_Email varchar(50) NOT NULL,
-    Order_Date_Time DATETIME NOT NULL,
+    Order_Date_Time DATETIME NOT NULL UNIQUE,
     Staff_Email varchar(50) NOT NULL,
     PRIMARY KEY (Customer_Email, Order_Date_Time),
     FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email));
     
  CREATE TABLE IF NOT EXISTS Staff(
-    Staff_Email varchar(40) NOT NULL PRIMARY KEY,
+    Staff_Email varchar(40) NOT NULL UNIQUE PRIMARY KEY,
     Staff_FirstName varchar(20) NOT NULL,
     Staff_LastName varchar(20) NOT NULL);
     
-///////////////DOESN'T WORK///////////////////////////////////////////////
 CREATE TABLE IF NOT EXISTS Order_Details(
     Customer_Email varchar(50) NOT NULL,
     Order_Date_Time DATETIME NOT NULL,
@@ -41,4 +41,3 @@ CREATE TABLE IF NOT EXISTS Order_Details(
     FOREIGN KEY (Customer_Email) REFERENCES Customers(Customer_Email),
     FOREIGN KEY (Order_Date_Time) REFERENCES Orders(Order_Date_Time),
     FOREIGN KEY (Service_Name) REFERENCES Services(Service_Name));
-//////////////////////////////////////////////////////////////////////////////
